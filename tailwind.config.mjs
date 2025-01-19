97cd6368c70a6264ccd8025fe680e2be426b7396
Ekernik <ekernik@gmail.com>
2025-01-19 14:50:39 -0600

    feat: extend color theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'hsl(var(--color-primary) / <alpha-value>)',
+        'primary-foreground':
+          'hsl(var(--color-primary-foreground) / <alpha-value>)',
+        muted: 'hsl(var(--color-muted) / <alpha-value>)',
+        'muted-foreground':
+          'hsl(var(--color-muted-foreground) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 };
