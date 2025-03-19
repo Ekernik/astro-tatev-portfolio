1a7aa6160c8e6bc44039707ee5aa911fa76d12b4
Ekernik <ekernik@gmail.com>
2025-03-18 21:48:16 -0400

    feat: new sliding images and links

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,11 +1,14 @@
 import Autoplay from 'embla-carousel-autoplay';
 import useEmblaCarousel from 'embla-carousel-react';
-import { useEffect } from 'react';
 import profile_1 from '../assets/profile_1.jpg';
 import profile_1_sm from '../assets/profile_1_sm.jpg';
 import profile_2 from '../assets/profile_2.jpg';
 import profile_3 from '../assets/profile_3.png';
 import profile_4 from '../assets/profile_4.jpg';
+import profile_5 from '../assets/profile_5.jpg';
+import profile_6 from '../assets/profile_6.jpg';
+import profile_7 from '../assets/profile_7.jpg';
+import profile_8 from '../assets/profile_8.jpg';
 
 const images = [
   {
@@ -14,19 +17,17 @@ const images = [
     bigSrc: profile_1.src,
     alt: 'A close-up portrait of Tatev, a violinist with long, flowing brown hair and expressive dark eyes. She smiles warmly while looking directly at the camera, holding her violin gracefully in her hands. The background features a soft, blurred backdrop of lush green foliage from trees, creating a serene and natural atmosphere.',
   },
-  { id: 2, smallSrc: profile_2.src, bigSrc: profile_2.src, alt: '' },
-  { id: 3, smallSrc: profile_3.src, bigSrc: profile_3.src, alt: '' },
-  { id: 4, smallSrc: profile_4.src, bigSrc: profile_4.src, alt: '' },
+  { id: 2, smallSrc: profile_5.src, bigSrc: profile_5.src, alt: '' },
+  { id: 3, smallSrc: profile_2.src, bigSrc: profile_2.src, alt: '' },
+  { id: 4, smallSrc: profile_8.src, bigSrc: profile_8.src, alt: '' },
+  { id: 5, smallSrc: profile_3.src, bigSrc: profile_3.src, alt: '' },
+  { id: 6, smallSrc: profile_6.src, bigSrc: profile_6.src, alt: '' },
+  { id: 7, smallSrc: profile_4.src, bigSrc: profile_4.src, alt: '' },
+  { id: 8, smallSrc: profile_7.src, bigSrc: profile_7.src, alt: '' },
 ];
 
 export default function ProfileImage() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
-
-  useEffect(() => {
-    if (emblaApi) {
-      console.log(emblaApi.slideNodes()); // Access API
-    }
-  }, [emblaApi]);
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
 
   return (
     <div
@@ -55,15 +56,15 @@ function ImageCard({
   };
 }) {
   return (
-    <div className='min-w-0 flex-grow-0 flex-shrink-0 basis-full'>
+    <div className='min-w-0 w-full flex-grow-0 flex-shrink-0 basis-full aspect-[5/6] md:aspect-[9/13]'>
       <img
         src={image.smallSrc}
-        className='w-full rounded-lg shadow-md md:hidden aspect-[5/6] md:aspect-[9/13] object-cover'
+        className='w-full h-full rounded-lg shadow-md md:hidden object-cover'
         alt={image.alt}
       />
       <img
         src={image.bigSrc}
-        className='hidden md:block rounded-lg shadow-md aspect-[5/6] md:aspect-[9/13] object-cover'
+        className='hidden md:block w-full h-full rounded-lg shadow-md object-cover'
         alt={image.alt}
       />
     </div>
